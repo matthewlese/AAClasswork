815c8e2dc7187b9c8e323c321792c92af4a63606
RobertT122 <robert@tetrault.org>
2021-09-27 18:23:42 -0400

    Hash_set

--- Week 5/D1/MyHashMap/lib/p03_hash_set.rb
+++ Week 5/D1/MyHashMap/lib/p03_hash_set.rb
@@ -7,18 +7,30 @@ class HashSet
   end
 
   def insert(key)
+    unless self.include?(key)
+      resize! unless count < num_buckets
+      self[key].push(key) 
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    idx = num.hash % num_buckets
+    @store[idx]
   end
 
   def num_buckets
@@ -26,5 +38,9 @@ class HashSet
   end
 
   def resize!
+    temp = @store.flatten
+    @store = Array.new(num_buckets * 2)  { Array.new }
+    @count = 0
+    temp.each { |val| self.insert(val) }
   end
 end
