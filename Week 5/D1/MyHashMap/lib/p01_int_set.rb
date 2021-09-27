393e2c4a9752c329de23dd8a682bf7a31e4bbda4
Matthew Lese <matthewjlese@gmail.com>
2021-09-27 14:00:40 -0700

    adds MaxIntSet and IntSet functionality

--- Week 5/D1/MyHashMap/lib/p01_int_set.rb
+++ Week 5/D1/MyHashMap/lib/p01_int_set.rb
@@ -1,19 +1,29 @@
 class MaxIntSet
+
+  attr_reader :store, :max
+  
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    store[num] = true
   end
 
   def remove(num)
+    store[num] = false
   end
 
   def include?(num)
+    store[num]
   end
 
   private
 
   def is_valid?(num)
+    num >= 0 && num < max
   end
 
   def validate!(num)
@@ -22,23 +32,32 @@ end
 
 
 class IntSet
+
+  attr_reader :num_buckets, :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    self[num].push(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    idx = num % num_buckets
+    store[idx]
   end
 
   def num_buckets
