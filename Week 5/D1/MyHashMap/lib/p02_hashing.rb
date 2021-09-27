b70da0fa67ba228c9703c78aa4b249218d0b93c9
Matthew Lese <matthewjlese@gmail.com>
2021-09-27 15:10:18 -0700

    completes p02_hashing

--- Week 5/D1/MyHashMap/lib/p02_hashing.rb
+++ Week 5/D1/MyHashMap/lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    return 1375098234589.hash if empty?
+    inject(2) do |acc, ele|
+      (acc + ele.hash).hash
+    end
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +21,9 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    var = to_a.inject(3) do |acc, ele|
+      (acc + ele.hash)
+    end
+    var.hash
   end
 end
