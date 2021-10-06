01bd4d864e74ad0b3f0edda8239e806ed6f31c3b
Matthew Lese <matthewjlese@gmail.com>
2021-10-06 15:26:37 -0700

    completes first controller until nesting parameters

--- Week 6/D2/build_your_own_active_record/lib/01_sql_object.rb
+++ Week 6/D2/build_your_own_active_record/lib/01_sql_object.rb
@@ -5,18 +5,18 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    
   end
 
   def self.finalize!
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    self.name.tableize
   end
 
   def self.all
