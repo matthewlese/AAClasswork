1bb746f66f2898f93e7aeb5f36c3c8b042a7da0c
Matthew Lese <matthewjlese@gmail.com>
2021-10-08 15:25:08 -0700

    adds show view

--- Week 6/D5/NinetyNineCats/db/seeds.rb
+++ Week 6/D5/NinetyNineCats/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cats = Cat.create([{birth_date: "2015/01/20", color: "cyan", name: "Mark", sex: "M", description: "first cat"},
+{birth_date: "2021/01/20", color: "zebra", name: "Andy", sex: "M", description: "in honor of Andy Huang"}
+])
