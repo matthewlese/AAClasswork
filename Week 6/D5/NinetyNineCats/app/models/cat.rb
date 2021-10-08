57ccf958b3742f3046947c08c52909a0d4a4ebc8
Abraham Fong <abisfong@ucsc.edu>
2021-10-08 14:59:54 -0700

    Begin cat controller and view

--- Week 6/D5/NinetyNineCats/app/models/cat.rb
+++ Week 6/D5/NinetyNineCats/app/models/cat.rb
@@ -1,8 +1,13 @@
-require action_view
+require 'action_view'
 
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   validates :birth_date, :color, :name, :sex, :description, presence: true
   validates :color, inclusion: { in: ['cyan', 'red', 'zebra', 'gold', 'invisible', 'white', 'black']}
   validates :sex, inclusion: { in: ['M', 'F']}
 
+  def age
+    time_ago_in_words(self.birth_date)
+  end
 end
