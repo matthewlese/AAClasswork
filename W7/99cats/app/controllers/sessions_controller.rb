bf8e772af52df48fe7007d6c8d7a562b6be262e5
Matthew Lese <matthewjlese@gmail.com>
2021-10-11 15:11:30 -0700

    completes view for controller

---
+++ W7/99cats/app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+    if @user
+      login(@user)
+      redirect_to cats_url
+    else
+      render json: ["Invalid username or password"]
+    end
+  end
+
+end
--- W7/99cats/config/routes.rb
+++ W7/99cats/config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create]
 
+  resource :sessions, only: [:new, :create, :destroy]
+
   root to: redirect('/cats')
 end
