f174892275efb321932139c69cc58fa28764752f
Matthew Lese <matthewjlese@gmail.com>
2021-10-11 16:32:33 -0700

    adds required_logged_in/out and before actions

--- W7/99cats/app/controllers/application_controller.rb
+++ W7/99cats/app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::Base
     @current_user = nil
   end
 
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def require_logged_out
+    redirect_to users_url if logged_in?
+  end
+
 end
