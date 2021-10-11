35e1d82e55f94b5b3f5695add9cf1d2ec5d3ab1c
Matthew Lese <matthewjlese@gmail.com>
2021-10-11 14:22:35 -0700

    completes through is_password? method

---
+++ W7/99cats/app/models/user.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :user_name, :password_digest, :session_token, presence: true
+  validates :user_name, :session_token, uniqueness: true
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_valid_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+
+
+end
