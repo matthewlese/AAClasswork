34ed3b39ecf89f56adae7ddb4f08187be53f1beb
Matthew Lese <matthewjlese@gmail.com>
2021-10-13 14:35:53 -0700

    passes first validation spec

---
+++ W7/D2/MusicApp/app/controllers/bands_controller.rb
@@ -0,0 +1,40 @@
+class BandsController < ApplicationController
+
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @band = Band.find_by(band_params[:name])
+    if @band
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+
+end
