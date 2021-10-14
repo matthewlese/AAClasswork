2ebfc13b7357965cf9dc1ef6827a1717018dc577
Emmett He <emmetthe26@gmail.com>
2021-10-13 17:27:04 -0700

    Made show and index html files

--- W7/D3/GoalsApp/spec/spec_helper.rb
+++ W7/D3/GoalsApp/spec/spec_helper.rb
@@ -96,9 +96,9 @@ RSpec.configure do |config|
 end
 
 def login_user(user)
-  visit new_session_url
+  visit new_user_url
   fill_in('username', with: user.username)
   fill_in('password', with: 'password')
-  click_button('Log In')
+  click_button('Sign Up')
 end
 
