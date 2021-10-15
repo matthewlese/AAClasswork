f338ac0d1c48870e9ced89487751621522e22ceb
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-10-15 15:23:05 -0700

    Work on project

--- W7/D5/RedditClone/app/controllers/users_controller.rb
+++ W7/D5/RedditClone/app/controllers/users_controller.rb
@@ -1,13 +1,48 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    # @user = User.find_by(username: params[:username]) # may want to use find(:id)
+    @user = User.find(params[:id])
+    render :show
   end
 
   def index
+    @users = User.all
+    render :index
   end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
