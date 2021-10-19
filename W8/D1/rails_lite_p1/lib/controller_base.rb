4fda0ae7b0a616d8d01aade42a1a3c016cbc925b
Julian Erville <jerville@gmail.com>
2021-10-18 20:15:18 -0400

    Add controller server

--- W8/D1/rails_lite_p1/lib/controller_base.rb
+++ W8/D1/rails_lite_p1/lib/controller_base.rb
@@ -8,20 +8,35 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    if @already_built_response
+      return true
+    else
+      return false
+    end
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Already rendered" if already_built_response?
+    @res.status = 302
+    @res.location = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Already rendered" if already_built_response?
+    @res.content_type = content_type
+    @res.body = [content]
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
@@ -38,3 +53,6 @@ class ControllerBase
   end
 end
 
+# redirect_app = Proc.new do
+#   ['302', {'Content-Type' => 'text/html', 'Location' => 'https://dasjkhkjashd'}, ['hello world']]
+# end
