d9e9d70f03e8ea75ebae1a027e298f66ca7c072b
jacky <jc6825@stern.nyu.edu>
2021-10-26 17:28:47 -0400

    Complete setup

--- W9/D2/tictactoe/src/ttt-view.js
+++ W9/D2/tictactoe/src/ttt-view.js
@@ -2,8 +2,32 @@ class View {
   constructor(game, el) {}
 
   setupBoard() {
-    
+    const grid = document.createElement('ul'); 
+    for (let i = 1; i <= 9; i++) {
+      let li = document.createElement('li');
+      li.setAttribute('data-row', Math.floor(i / 3)); 
+      li.setAttribute('data-col', (i % 3)); 
+      li.style.borderStyle = "solid";
+      li.style.backgroundColor = "grey";
+      li.classList.add('square');
+      grid.appendChild(li); 
+    }
+    grid.style.display = "flex"; //flex gives fixed width 
+    grid.style.flexWrap = "wrap";
+
+    //Create event listener for hover:
+    const squares = document.querySelectorAll('.square'); 
+    squares.forEach(square => {
+      square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "yellow"; 
+      })
+      //accounting for when a user stops hovering:
+      square.addEventListener('mouseout', () => {
+        square.style.backgroundColor = "grey"; 
+      })
+    })
   }
+
   
   bindEvents() {}
 
