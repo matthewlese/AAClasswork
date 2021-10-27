36813d0aae57438358d80986301a31a905a41331
Matthew Lese <matthewjlese@gmail.com>
2021-10-27 14:29:19 -0700

    starts FollowToggle constructor

---
+++ W9/D3/ajaxTwitter/frontend/follow_toggle.js
@@ -0,0 +1,19 @@
+class FollowToggle {
+  constructor() {
+    
+  }
+}
+
+// const saveGif = e => {
+//   const $input = $('#save-gif-title');
+//   const title = $input.val();
+//   $input.val('');
+// }
+
+// const setEventListeners = () => {
+//   $('#new-gif-form').on('submit', e => {
+//     e.preventDefault();
+//     // Fetch a new GIF
+//     fetchNewGif();
+//   });
+// }
