951da904fd2c811a69da5c3cbe50e34ceedd83f6
Cal Brown <calsbrown4@gmail.com>
2021-11-05 16:42:41 -0700

    Clock

--- W10/D5/ReactWidgets/clock.jsx
+++ W10/D5/ReactWidgets/clock.jsx
@@ -1,8 +1,4 @@
 import React from "react"
-<<<<<<< HEAD
-=======
-
->>>>>>> 319504152fb677b31ed0ae7b61158ff675b6a145
 class Clock extends React.Component {
   constructor(props) { //first
     super(props)
@@ -10,13 +6,11 @@ class Clock extends React.Component {
       date: 0
     }
     this.tick = this.tick.bind(this)
-<<<<<<< HEAD
   }
   tick() {
     this.setState({ date: this.state.date + 1 })
     // this.props.tick()
   }
-
   render() {
     return (
       <div>Our Clock</div> //second
@@ -26,25 +20,4 @@ class Clock extends React.Component {
     setInterval(this.tick(), 1000)
   }
 }
-=======
-  }
-
-  tick() {
-    this.setState({date: this.state.date + 1})
-    // this.props.tick()
-  }
-
-  
-  render(){
-    return (
-      <div>Our Clock</div> //second
-      )
-    }
-
-  componentDidMount() { // third
-    setInterval(this.tick(), 1000)
-  }
-}
-
 export default Clock
->>>>>>> 319504152fb677b31ed0ae7b61158ff675b6a145
