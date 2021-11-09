58823580abe8d73ad7b321159229f16e8d9fcaa9
mjlomeli <mjlomeli@uci.edu>
2021-11-09 14:55:20 -0800

    finished phase 3 or 2 i forgot

--- W11/D2/todos/frontend/reducers/todos_reducer.js
+++ W11/D2/todos/frontend/reducers/todos_reducer.js
@@ -1,3 +1,6 @@
+import {RECEIVE_TODOS, RECEIVE_TODO} from "../actions/todo_actions"
+import { receiveTodo, receiveTodos } from "../actions/todo_actions";
+
 const initialState = {
   1: {
     id: 1,
@@ -15,9 +18,15 @@ const initialState = {
 
 const todosReducer = (state = initialState, action) => {
   Object.freeze(state)
-  const nextState = Object.assign({}, state)
+  let nextState = {};
   switch (action.type) {
     // will have other cases
+    case RECEIVE_TODO:
+      nextState = Object.assign({}, state) // not accounting for pre-existing
+      return nextState[action.todo.id] = action.todo
+    case RECEIVE_TODOS:
+      // foreach then recur
+      return nextState[action.todos.id] = action.todos
     default:
       return state;
   }
--- W11/D2/todos/frontend/todo_redux.jsx
+++ W11/D2/todos/frontend/todo_redux.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import configureStore from './store/store'
+import {receiveTodo, receiveTodos} from './actions/todo_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
 	const content = document.getElementById("content")
   const store = configureStore()
 
   window.store = store
+  window.receiveTodos = receiveTodos;
+  window.receiveTodo = receiveTodo;
 	
 	ReactDOM.render(<h1>Todos App</h1>, content)
 })
