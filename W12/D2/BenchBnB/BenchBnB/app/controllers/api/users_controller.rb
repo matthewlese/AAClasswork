00e545117ff30af258d7cb8a82ab0b47faa95d07
ecbell <ebell691@gmail.com>
2021-11-16 16:51:07 -0600

    working through phase 1

--- W12/D2/BenchBnB/BenchBnB/app/controllers/api/users_controller.rb
+++ W12/D2/BenchBnB/BenchBnB/app/controllers/api/users_controller.rb
@@ -1,2 +1,15 @@
 class Api::UsersController < ApplicationController
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      render :show
+    else
+      render json: @user.error.full_messages, status: 401
+    end
+  end
+
+  def user_params 
+    params.require(:user).permit(:username, :password)
+  end
 end
