b0795c39dd1976de6fea8e45392750df725f2628
ecbell <ebell691@gmail.com>
2021-11-16 19:14:07 -0600

    finish reducers

---
+++ W12/D2/BenchBnB/BenchBnB/frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions'
+
+export default (state={}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_ERRORS:
+      return Object.assign({}, state, {errors: action.errors} )
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, {errors: null})
+    default:
+      return state;
+  }
+}
