01e8f11f2b032f8c147e27ab021c900fea1ce83a
Matthew Lese <matthewjlese@gmail.com>
2021-12-04 14:58:25 -0800

    completes setup

--- W14/MERNtwitter/app.js
+++ W14/MERNtwitter/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+const db = require('./config/keys').mongoURI
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello World 2"))
 
