58221809512636816eebb2c8ea3dfc714a2522bd
Matthew Lese <matthewjlese@gmail.com>
2021-12-04 18:00:12 -0800

    completes through using mongodb

--- W14/MERNtwitter/app.js
+++ W14/MERNtwitter/app.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+
 const db = require('./config/keys').mongoURI
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
-  
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World 2"))
+app.get("/", (req, res) => {
+  console.log(res)
+  res.send("Hello World 2")
+})
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
